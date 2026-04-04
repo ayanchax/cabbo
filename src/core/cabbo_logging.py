76583a7c89ccdb5a55fd223fb2e2079e79dbc207
ayanchax <42091907+ayanchax@users.noreply.github.com>
2026-04-05 02:38:04 +0530

    fix: update Dockerfile.dev to use workers for FastAPI and modify logging level in cabbo_logging.py

--- src/core/cabbo_logging.py
+++ src/core/cabbo_logging.py
@@ -10,11 +10,11 @@ os.makedirs(LOG_DIR, exist_ok=True)
 # Prevent duplicate handlers (important for reload environments)
 logger = logging.getLogger(APP_NAME)
 if not logger.handlers:
-    cabbo_debug_handler = TimedRotatingFileHandler(
-        os.path.join(LOG_DIR, 'debug.log'), when='midnight', interval=1, backupCount=15, encoding='utf-8', delay=True
-    )
-    cabbo_debug_handler.setLevel(logging.DEBUG)
-    cabbo_debug_handler.setFormatter(logging.Formatter(f'%(asctime)s [%(levelname)s] {APP_NAME} :: %(name)s: %(message)s'))
+    # cabbo_debug_handler = TimedRotatingFileHandler(
+    #     os.path.join(LOG_DIR, 'debug.log'), when='midnight', interval=1, backupCount=15, encoding='utf-8', delay=True
+    # )
+    # cabbo_debug_handler.setLevel(logging.DEBUG)
+    # cabbo_debug_handler.setFormatter(logging.Formatter(f'%(asctime)s [%(levelname)s] {APP_NAME} :: %(name)s: %(message)s'))
 
     cabbo_error_handler = TimedRotatingFileHandler(
         os.path.join(LOG_DIR, 'error.log'), when='midnight', interval=1, backupCount=15, encoding='utf-8', delay=True
@@ -27,9 +27,9 @@ if not logger.handlers:
     console_handler.setFormatter(logging.Formatter(f'%(asctime)s [%(levelname)s] {APP_NAME} :: %(name)s: %(message)s'))
 
     root_logger = logging.getLogger()  # root logger to catch any logs from libraries that don't use the app logger
-    root_logger.setLevel(logging.DEBUG)
+    root_logger.setLevel(logging.INFO)
     root_logger.addHandler(console_handler)
-    root_logger.addHandler(cabbo_debug_handler)
+    #root_logger.addHandler(cabbo_debug_handler)
     root_logger.addHandler(cabbo_error_handler)
 
 # Optionally, set root logger to propagate if you want logs everywhere
